4de86330199801722ea14b0ee889c0edec8cbf1a
Christian S <cschmitz7@icloud.com>
2015-05-07 15:45:57 -0300

    facebook login funcionando (mas ainda não linkando ao parse)

--- AnimalRescue/AnimalRescue/ViewController.swift
+++ AnimalRescue/AnimalRescue/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var loginButton: FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +25,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
