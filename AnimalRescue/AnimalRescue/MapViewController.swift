7d8d50f6f358b5254fa2f4e60657d610df4daf3b
Rodrigo Scroferneker <rodrigo.scroferneker@gmail.com>
2015-05-07 14:57:42 -0300

    nd

---
+++ AnimalRescue/AnimalRescue/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MapViewController.swift
+//  AnimalRescueMapViewController
+//
+//  Created by Rodrigo Franzoi Scroferneker on 07/05/15.
+//  Copyright (c) 2015 Christian S. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+      
+    }
+    
+    
+}
