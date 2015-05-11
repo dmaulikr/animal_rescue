fcf24a3c7f35f12271bc23ccef8c61bebec385be
Rodrigo Scroferneker <rodrigo.scroferneker@gmail.com>
2015-05-11 12:48:50 -0300

    mapa

--- AnimalRescue/AnimalRescue/MapViewController.swift
+++ AnimalRescue/AnimalRescue/MapViewController.swift
@@ -19,8 +19,7 @@ class MapViewController: UIViewController , CLLocationManagerDelegate , MKMapVie
     
     
     var animals: NSMutableArray!
-    var localizaçao: CLLocationCoordinate2D!
-    
+    var points: NSMutableArray = NSMutableArray()
     
     @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
@@ -29,40 +28,39 @@ class MapViewController: UIViewController , CLLocationManagerDelegate , MKMapVie
         super.viewDidLoad()
         mapView.delegate = self
         
+        
+        
         locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
         generatePins()
-
-        
-        
-    }
-    
-    func retrieveAnimalsFromParse(){
-    
     }
     
     func generatePins()
     {
         mapView.showsUserLocation = true
-
+        
         var point = MKPointAnnotation();
         point.coordinate = CLLocationCoordinate2DMake(-30.055548895017466, -51.1751056972908)
         point.title = "Animal Aprisionado"
         point.subtitle = "Um animal precisa de sua ajuda!"
-        
+        points.addObject(point)
         mapView.addAnnotation(point)
-        mapView.showAnnotations([point], animated: true)
         
-        if(updateDistanceAnnotation(point))
-        {
-            var localNotification: UILocalNotification = UILocalNotification()
-            localNotification.alertAction = "Testing notifications on iOS8"
-            localNotification.alertBody = "Woww it works!!"
-            localNotification.fireDate = NSDate(timeIntervalSinceNow: 2)
-            UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
-
+        var point1 = MKPointAnnotation();
+        point1.coordinate = CLLocationCoordinate2DMake(-30.054478, -51.196474)
+        point1.title = "Animal Aprisionado"
+        point1.subtitle = "Um animal precisa de sua ajuda!"
+        
+        points.addObject(point)
+        mapView.addAnnotation(point)
+        
+        points.addObject(point1)
+        mapView.addAnnotation(point1)
+        
+        for(var i = 0; i < points.count; i++){
+            mapView.showAnnotations([points[i]], animated: true)
         }
-    
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -77,7 +75,28 @@ class MapViewController: UIViewController , CLLocationManagerDelegate , MKMapVie
             if let location = locationManager.location?.coordinate {
                 mapView.setCenterCoordinate(location, animated: true)
                 mapView.camera.altitude = pow(2, 11)
-               
+                
+                for(var i = 0; i<points.count; i++){
+                    if(updateDistanceAnnotation(points[i] as! MKPointAnnotation))
+                    {
+                        
+                        locationManager.delegate = self
+                        locationManager.requestAlwaysAuthorization()
+                        
+                        println("potato")
+                        var localNotification: UILocalNotification = UILocalNotification()
+                        localNotification.alertAction = "Animal Rescue"
+                        localNotification.alertBody = "Animal em perigo por perto!!!"
+                        localNotification.fireDate = NSDate(timeIntervalSinceNow: 2)
+                        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+                        
+                        var alert = UIAlertController(title: "Animal por perto", message: "Animal em perigo por perto!", preferredStyle: UIAlertControllerStyle.Alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+                        self.presentViewController(alert, animated: true, completion: nil)
+                        
+                    }
+                }
+                
             } else {
                 locationManager.startUpdatingLocation()
             }
@@ -87,17 +106,16 @@ class MapViewController: UIViewController , CLLocationManagerDelegate , MKMapVie
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         
         locationManager.stopUpdatingLocation()
-        
         if let location = locations.last as? CLLocation {
             mapView.setCenterCoordinate(location.coordinate, animated: true)
             mapView.camera.altitude = pow(2, 11)
-
+            
         }
     }
     
-        func updateDistanceAnnotation(annotation: MKAnnotation!) -> (Bool)
+    func updateDistanceAnnotation(annotation: MKPointAnnotation!) -> (Bool)
     {
-
+        
         if (annotation == nil)
         {
             println("No annotation selected")
@@ -118,6 +136,45 @@ class MapViewController: UIViewController , CLLocationManagerDelegate , MKMapVie
         
         if(distance > 600) {return false}
         else {return true}
+        
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        var identifier = "CustomAnnotation"
+        
+        if annotation.isKindOfClass(MKUserLocation) {
+            return nil
+        }
+        
+        if annotation.isKindOfClass(MKPointAnnotation) {
+            var pin = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+            
+            if pin == nil {
+                
+                
+                pin = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                pin.image = UIImage(named: "cageBlank")
+                pin.centerOffset = CGPointMake(0, -10)
+                pin.canShowCallout = true
+                
+                
+ 
+                var button = UIButton.buttonWithType(.DetailDisclosure) as! UIButton
+                pin!.leftCalloutAccessoryView = button
+                
+                var image = UIImageView(image: UIImage(named: "cageBlank"))
+                pin!.rightCalloutAccessoryView = image
+                
+                
+            } else {
+                pin!.annotation = annotation
+            }
+            
+            return pin
+        }
+        
+        return nil
         
     }
     
