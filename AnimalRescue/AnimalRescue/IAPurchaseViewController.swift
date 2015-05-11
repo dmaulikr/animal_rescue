d09150cf8474da880d5478082fd8ad3b801e1428
Christian S <cschmitz7@icloud.com>
2015-05-11 16:26:17 -0300

    provisioning profile - tentativa 1

--- AnimalRescue/AnimalRescue/IAPurchaseViewController.swift
+++ AnimalRescue/AnimalRescue/IAPurchaseViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 import Foundation
+import Parse
 import StoreKit
 
 
-class IAPurchaseViewController: UIViewController, SKProductsRequestDelegate {
+class IAPurchaseViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    
+    var list = [SKProduct]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let productsRequest = SKProductsRequest(productIdentifiers: ["produto"])
-        productsRequest.delegate = self;
-        productsRequest.start();
+        
+        SKPaymentQueue.defaultQueue().addTransactionObserver(self)
+        
+        if(SKPaymentQueue.canMakePayments()) {
+            println("IAP is enabled, loading")
+            var productID:NSSet = NSSet(objects: "com.CRRW.AnimalRescue.Buy10Keys")
+            var request: SKProductsRequest = SKProductsRequest(productIdentifiers: productID as Set<NSObject>)
+            request.delegate = self
+            request.start()
+        } else {
+            println("please enable IAPS")
+            let alert = UIAlertView()
+            alert.title = "ERRO"
+            alert.message = "Houve um problema ao carregar a compra"
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+            
+        }
     }
     
     
@@ -26,8 +44,59 @@ class IAPurchaseViewController: UIViewController, SKProductsRequestDelegate {
     }
     
     
-    @IBAction func BuyButtonPressed(sender: AnyObject) {
+    func paymentQueue(queue: SKPaymentQueue!, updatedTransactions transactions: [AnyObject]!) {
+        for transaction:AnyObject in transactions {
+            if let trans:SKPaymentTransaction = transaction as? SKPaymentTransaction {
+                switch trans.transactionState {
         
+                    case .Purchasing:
+                        println("Purchasing Product");
+                    break
+        
+                    case .Failed:
+                        println("Purchase Failed");
+                    break
+        
+                    case .Restored:
+                        println("Product Restored");
+                    break
+        
+                    case .Purchased:
+                        println("Product Purchased");
+                        SKPaymentQueue.defaultQueue().finishTransaction(transaction as! SKPaymentTransaction)
+                    break
+        
+                    default:
+                    break
+                };
+            }
+        }
+    }
+    
+    
+    @IBAction func BuyButtonPressed(sender: AnyObject) {
+                        
+//        ￼if (SKPaymentQueue.canMakePayments()) {
+//                let product = response.products[0] as! SKProduct
+//                var payment = SKPayment(product: product)
+//                SKPaymentQueue.defaultQueue().addPayment(payment);
+//        } else {
+//            println("Not allowed to buy")
+//        }
+    }
+    
+    
+    func addCoins (){
+//        var user = PFUser.currentUser()
+//        
+//        if (user?.objectForKey("keys") >= 1) {
+//            println("Animal libertado")
+//            user?.objectForKey("keys") = user?.objectForKey("keys") - 1
+//            (user["animalsReleased"] as NSArray).append
+//        } else {
+//            println("você não tem keys suficientes")
+//        }
+//        
         
     }
     
