5f6e18c222d43dd5dad57a30396b330b855471ec
Christian S <cschmitz7@icloud.com>
2015-05-12 14:16:50 -0300

    Código da IAPurchase pronto

--- AnimalRescue/AnimalRescue/IAPurchaseViewController.swift
+++ AnimalRescue/AnimalRescue/IAPurchaseViewController.swift
@@ -14,35 +14,67 @@ import StoreKit
 
 class IAPurchaseViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
     
-    var list = [SKProduct]()
+//    var list = [SKProduct]()
+
+    var product_id: NSString?;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         println(PFUser.currentUser())
         
+        product_id = "com.CRRW.AnimalRescue.10Keys"
+        
         SKPaymentQueue.defaultQueue().addTransactionObserver(self)
         
-        if(SKPaymentQueue.canMakePayments()) {
-            println("IAP is enabled, loading")
-            var productID:NSSet = NSSet(objects: "com.CRRW.AnimalRescue.Buy10Keys")
-            var request: SKProductsRequest = SKProductsRequest(productIdentifiers: productID as Set<NSObject>)
-            request.delegate = self
-            request.start()
-        } else {
-            println("please enable IAPS")
-            let alert = UIAlertView()
-            alert.title = "ERRO"
-            alert.message = "Houve um problema ao carregar a compra"
-            alert.addButtonWithTitle("Ok")
-            alert.show()
-            
-        }
+//
+//        if(SKPaymentQueue.canMakePayments()) {
+//            println("IAP is enabled, loading")
+//            var productID:NSSet = NSSet(objects: "com.CRRW.AnimalRescue.10Keys")
+//            var request: SKProductsRequest = SKProductsRequest(productIdentifiers: productID as Set<NSObject>)
+//            request.delegate = self
+//            request.start()
+//        } else {
+//            println("please enable IAPS")
+//            let alert = UIAlertView()
+//            alert.title = "ERRO"
+//            alert.message = "Houve um problema ao carregar a compra"
+//            alert.addButtonWithTitle("Ok")
+//            alert.show()
+//            
+//        }
+    }
+    
+    func buyProduct(product: SKProduct){
+        println("Sending the Payment Request to Apple");
+        var payment = SKPayment(product: product)
+        SKPaymentQueue.defaultQueue().addPayment(payment);
+        
     }
     
     
     func productsRequest(request: SKProductsRequest!, didReceiveResponse response: SKProductsResponse!) {
         println(response) // [SKProduct]
+        
+        var count : Int = response.products.count
+        if (count > 0) {
+            var validProducts = response.products
+            var validProduct: SKProduct = response.products[0] as! SKProduct
+            if (validProduct.productIdentifier == self.product_id) {
+                println(validProduct.localizedTitle)
+                println(validProduct.localizedDescription)
+                println(validProduct.price)
+                buyProduct(validProduct);
+            } else {
+                println(validProduct.productIdentifier)
+            }
+        } else {
+            println("nothing")
+        }
+    }
+    
+    func request(request: SKRequest!, didFailWithError error: NSError!) {
+        println("Error Fetching product information");
     }
     
     
@@ -52,19 +84,20 @@ class IAPurchaseViewController: UIViewController, SKProductsRequestDelegate, SKP
                 switch trans.transactionState {
         
                     case .Purchasing:
-                        println("Purchasing Product");
+                        println("Purchasing Product")
                     break
         
                     case .Failed:
-                        println("Purchase Failed");
+                        println("Purchase Failed")
+                        SKPaymentQueue.defaultQueue().finishTransaction(transaction as! SKPaymentTransaction)
                     break
         
                     case .Restored:
-                        println("Product Restored");
+                        println("Product Restored")
                     break
         
                     case .Purchased:
-                        println("Product Purchased");
+                        println("Product Purchased")
                         SKPaymentQueue.defaultQueue().finishTransaction(transaction as! SKPaymentTransaction)
                     break
         
@@ -78,15 +111,18 @@ class IAPurchaseViewController: UIViewController, SKProductsRequestDelegate, SKP
     
     @IBAction func BuyButtonPressed(sender: AnyObject) {
                         
-//        ￼if (SKPaymentQueue.canMakePayments()) {
-//                let product = response.products[0] as! SKProduct
-//                var payment = SKPayment(product: product)
-//                SKPaymentQueue.defaultQueue().addPayment(payment);
-//        } else {
-//            println("Not allowed to buy")
-//        }
+        if (SKPaymentQueue.canMakePayments()) {
 
-        //
+            var productID:NSSet = NSSet(object: self.product_id!);
+            var productsRequest:SKProductsRequest = SKProductsRequest(productIdentifiers: productID as Set<NSObject>);
+            productsRequest.delegate = self;
+            productsRequest.start();
+            println("Fething Products");
+            
+        } else {
+            println("can't make purchases");
+        }
+        
     }
     
     
@@ -123,9 +159,10 @@ class IAPurchaseViewController: UIViewController, SKProductsRequestDelegate, SKP
         }
         
     }
+
+}
     
     
     
     
 
-}
